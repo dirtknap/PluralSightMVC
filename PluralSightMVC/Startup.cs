c711488a317b3bf88c697abc6e289c41c06b2446
Jeff Rector <jeff.rector@incontact.com>
2016-05-23 11:53:13 -0400

    Add project files.

---
+++ PluralSightMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluralSightMVC.Startup))]
+namespace PluralSightMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
