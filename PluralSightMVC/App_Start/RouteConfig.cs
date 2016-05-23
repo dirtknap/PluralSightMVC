0eda0efc4616ae6d2f1a52808640d0d210c0c348
Jeff Rector <jeff.rector@incontact.com>
2016-05-23 12:03:01 -0400

    controllers/actions section

--- PluralSightMVC/App_Start/RouteConfig.cs
+++ PluralSightMVC/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace PluralSightMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Cuisine",
+                url: "cuisine/{name}",
+                defaults: new {controller = "Cuisine", action = "Search", name = UrlParameter.Optional}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
